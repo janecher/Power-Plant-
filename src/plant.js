11e852dde6ac8f3b65d94d62308c8dc95fdc09bd
Ian Gregg <iangregg188@gmail.com>
2020-09-01 14:46:54 -0700

    working on the html and css elements of the application.
    co-authored by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

--- src/plant.js
+++ src/plant.js
@@ -59,20 +59,64 @@
   const plantTest = newPlant("flower");
   console.log(plantTest);
 
-  export {feed, hydrate, illuminate, overHydrate, superWater, scorch, highheat, changeName, newPlant}
+  export {feed, hydrate, illuminate, overHydrate, superWater, scorch, highheat, changeName, newPlant, stateControl, blueFood, redFood, greenFood }
 
   // $(document).ready(function() {
 
   // // This function has side effects because we are using jQuery. Manipulating the DOM will always be a side effect. Note that we only use one of our functions to alter soil. You can easily add more.
 
+  //   //Food(soil) button functions
   //   $('#feed').click(function() {
-  //     const newState = stateControl(blueFood);
+  //     const newState = stateControl(feed);
   //     $('#soil-value').text(`Soil: ${newState.soil}`);
   //   });
 
-  //   $('#feed').click(function() {
+  //   $('#redFoods').click(function() {
   //     const newState = stateControl(redFood);
   //     $('#soil-value').text('Soil: ${newState.soil}');
+  //   });
+
+  //   $('#blueFoods').click(function() {
+  //     const newState = stateControl(blueFood);
+  //     $('#soil-value').text('Soil: ${newState.soil}');
+  //   });
+
+  //   $('#greenFoods').click(function() {
+  //     const newState = stateControl(greenFood);
+  //     $('#soil-value').text('Soil: ${newState.soil}');
+  //   });
+
+  //   //Light functions
+
+  //   $('#illuminates').click(function() {
+  //     const newState = stateControl(illuminate);
+  //     $('#light-value').text('Light: ${newState.light}');
+  //   });
+
+  //   $('#scorcheds').click(function() {
+  //     const newState = stateControl(scorch);
+  //     $('#light-value').text('Light: ${newState.light}');
+  //   });
+
+  //   $('#highHeats').click(function() {
+  //     const newState = stateControl(highheat);
+  //     $('#light-value').text('Light: ${newState.light}');
+  //   });
+
+  //   //Water functions
+  //   $('#superWaters').click(function() {
+  //     const newState = stateControl(superWater);
+  //     $('#water-value').text('Water: ${newState.water}');
+  //   });
+
+  //   $('#hydrates').click(function() {
+  //     const newState = stateControl(hydrate);
+  //     $('#water-value').text('Water: ${newState.water}');
+  //   });
+
+  //   $('#overHydrates').click(function() {
+  //     const newState = stateControl(overHydrate);
+  //     $('#water-value').text('Water: ${newState.water}');
   //   });
 
   // // This function doesn't actually do anything useful in this application - it just demonstrates how we can "look" at the current state (which the DOM is holding anyway). However, students often do need the ability to see the current state without changing it so it's included here for reference.
