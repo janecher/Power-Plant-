6dbd20599d4fb13bea62ccc385609047a83c3021
Ian Gregg <iangregg188@gmail.com>
2020-09-01 14:02:48 -0700

    all the tests passed and we were able to create a new plant as well as name it.
    co-authored by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

--- _test_/powerplant.test.js
+++ _test_/powerplant.test.js
@@ -1,4 +1,4 @@
-import { feed, hydrate, illuminate, overHydrate, superWater, scorch, highheat } from '../src/plant.js';
+import { feed, hydrate, illuminate, overHydrate, superWater, scorch, highheat, newPlant, changeName } from '../src/plant.js';
 
 describe ('plant', () => {
 
@@ -8,52 +8,62 @@ describe ('plant', () => {
 
   //soil test
   test ('increment "soil" property by 1', () => {
-    let plant = { soil: 0, light: 0, water: 0 } 
+    let plant = { soil: 0, light: 0, water: 0, name: ""} 
     //const feed = changeState("soil")(1);
-    expect(feed(plant)).toMatchObject({soil: 1, light: 0, water: 0});
+    expect(feed(plant)).toMatchObject({soil: 1, light: 0, water: 0, name: ""});
   });
 
   //water test
   test ('increment "water" property by 1', () => {
-    let plant = {soil: 0, light: 0, water: 0 }
+    let plant = {soil: 0, light: 0, water: 0, name: ""}
     //const hydrate = changeState("water")(1);
-    expect(hydrate(plant)).toMatchObject({soil: 0, light: 0, water: 1});
+    expect(hydrate(plant)).toMatchObject({soil: 0, light: 0, water: 1, name: ""});
   });
 
   //light test
   test ('increment "light" property by 1', () => {
-    let plant = {soil: 0, light: 0, water: 0 }
+    let plant = {soil: 0, light: 0, water: 0, name: ""}
     //const hydrate = changeState("water")(1);
-    expect(illuminate(plant)).toMatchObject({soil: 0, light: 1, water: 0});
+    expect(illuminate(plant)).toMatchObject({soil: 0, light: 1, water: 0, name: ""});
   });
 
   test ('increment "light" property by -2', () => {
-    let plant = {soil: 0, light: 0, water: 0 }
+    let plant = {soil: 0, light: 0, water: 0, name: ""}
     //const hydrate = changeState("water")(1);
-    expect(scorch(plant)).toMatchObject({soil: 0, light: -2, water: 0});
+    expect(scorch(plant)).toMatchObject({soil: 0, light: -2, water: 0, name: ""});
   });
 
   test ('increment "light" property by 3', () => {
-    let plant = {soil: 0, light: 0, water: 0 }
+    let plant = {soil: 0, light: 0, water: 0, name: ""}
     //const hydrate = changeState("water")(1);
-    expect(highheat(plant)).toMatchObject({soil: 0, light: 3, water: 0});
+    expect(highheat(plant)).toMatchObject({soil: 0, light: 3, water: 0, name: ""});
   });
 
   test ('increment "light" property by 3', () => {
-    let plant = {soil: 0, light: 0, water: 0 }
+    let plant = {soil: 0, light: 0, water: 0, name: ""}
     //const hydrate = changeState("water")(1);
-    expect(highheat(plant)).toMatchObject({soil: 0, light: 3, water: 0});
+    expect(highheat(plant)).toMatchObject({soil: 0, light: 3, water: 0, name: ""});
   });
 
   test ('increment "water" property by -2', () => {
-    let plant = {soil: 0, light: 0, water: 0 }
+    let plant = {soil: 0, light: 0, water: 0, name: ""}
     //const hydrate = changeState("water")(1);
-    expect(overHydrate(plant)).toMatchObject({soil: 0, light: 0, water: -2});
+    expect(overHydrate(plant)).toMatchObject({soil: 0, light: 0, water: -2, name: ""});
   });
 
   test ('increment "water" property by 5', () => {
-    let plant = {soil: 0, light: 0, water: 0 }
+    let plant = {soil: 0, light: 0, water: 0, name: "" }
     //const hydrate = changeState("water")(1);
-    expect(superWater(plant)).toMatchObject({soil: 0, light: 0, water: 5});
+    expect(superWater(plant)).toMatchObject({soil: 0, light: 0, water: 5, name: ""});
+  });
+
+  test ('increment "name" be Bob', () => {
+    let plant = {soil: 0, light: 0, water: 0, name: ""}
+    let nameToChange = "Bob";
+    expect(changeName(nameToChange)(plant)).toMatchObject({soil: 0, light: 0, water: 0, name: "Bob"});
+  });
+
+  test ('newplant has been created', () => {
+    expect(newPlant("flower")).toMatchObject({soil: 0, light: 0, water: 0, name: "flower"});
   });
 });
--- src/plant.js
+++ src/plant.js
@@ -11,17 +11,26 @@
 
   const stateControl = storeState();
 
-  export const changeState = (prop) => {
+  const changeState = (prop) => {
     return (value) => {
       return (state) => ({
         ...state,
-        [prop] : (state[prop] || 0) + value
+        [prop] : (state[prop] || 0) + value 
       })
     }
   }
 
+  const changeNameState = (nameProp) => {
+    return (value) => {
+      return (state) => ({
+        ...state,
+        [nameProp] : value
+      })
+    }
+  }
+  
   // We create four functions using our function factory. We could easily create many more.
-
+  const changeName = changeNameState("name");
   const feed = changeState("soil")(1);
   const blueFood = changeState("soil")(5);
   const redFood = changeState("soil")(6);
@@ -35,28 +44,42 @@
   const scorch = changeState("light")(-2);
   const highheat = changeState("light")(3);
 
-  export {feed, hydrate, illuminate, overHydrate, superWater, scorch, highheat}
+  const soil = changeState("soil")(0);
+  const water = changeState("water")(0);
+  const light = changeState("light")(0);
 
-  $(document).ready(function() {
+  const newPlant = (name) => {
+    let plant = {
+      name
+    }
+    //let plant = storeState();
+    return {...plant, ...soil(plant), ...water(plant), ...light(plant)};
+  };
 
-  // This function has side effects because we are using jQuery. Manipulating the DOM will always be a side effect. Note that we only use one of our functions to alter soil. You can easily add more.
+  const plantTest = newPlant("flower");
+  console.log(plantTest);
 
-    $('#feed').click(function() {
-      const newState = stateControl(blueFood);
-      $('#soil-value').text(`Soil: ${newState.soil}`);
-    });
+  export {feed, hydrate, illuminate, overHydrate, superWater, scorch, highheat, changeName, newPlant}
 
-    $('#feed').click(function() {
-      const newState = stateControl(redFood);
-      $('#soil-value').text('Soil: ${newState.soil}');
-    });
+  // $(document).ready(function() {
 
-  // This function doesn't actually do anything useful in this application - it just demonstrates how we can "look" at the current state (which the DOM is holding anyway). However, students often do need the ability to see the current state without changing it so it's included here for reference.
+  // // This function has side effects because we are using jQuery. Manipulating the DOM will always be a side effect. Note that we only use one of our functions to alter soil. You can easily add more.
 
-    $('#show-state').click(function() {
-      // We just need to call stateControl() without arguments to see our current state.
-      const currentState = stateControl();
-      $('#soil-value').text(`Soil: ${currentState.soil}`);
-    });
-  });
-}
+  //   $('#feed').click(function() {
+  //     const newState = stateControl(blueFood);
+  //     $('#soil-value').text(`Soil: ${newState.soil}`);
+  //   });
+
+  //   $('#feed').click(function() {
+  //     const newState = stateControl(redFood);
+  //     $('#soil-value').text('Soil: ${newState.soil}');
+  //   });
+
+  // // This function doesn't actually do anything useful in this application - it just demonstrates how we can "look" at the current state (which the DOM is holding anyway). However, students often do need the ability to see the current state without changing it so it's included here for reference.
+
+  //   $('#show-state').click(function() {
+  //     // We just need to call stateControl() without arguments to see our current state.
+  //     const currentState = stateControl();
+  //     $('#soil-value').text(`Soil: ${currentState.soil}`);
+  //   });
+  // });
