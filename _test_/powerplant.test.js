242f398d5041274d59e5be919a700a7a8463d9b8
Ian Gregg <iangregg188@gmail.com>
2020-09-01 11:33:37 -0700

    had 3 tests pass testing.
    co-authored by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

--- _test_/powerplant.test.js
+++ _test_/powerplant.test.js
@@ -1,19 +1,30 @@
-import { plant } from '../src/plant.js';
+import { feed, hydrate, illuminate } from '../src/plant.js';
 
 describe ('plant', () => {
 
-  beforeEach(function(){
-    let plant = { soil: 0, light: 0, water: 0 } 
-  });
-
-  test ('increment "soil" property by 1', () => {
-    expect(feed(plant)).toMatchObject({});
-  });
+  // beforeEach(funnction(){
+  //   let plant = { soil: 0, light: 0, water: 0 } 
+  // });
 
   //soil test
+  test ('increment "soil" property by 1', () => {
+    let plant = { soil: 0, light: 0, water: 0 } 
+    //const feed = changeState("soil")(1);
+    expect(feed(plant)).toMatchObject({soil: 1, light: 0, water: 0});
+  });
 
   //water test
+  test ('increment "water" property by 1', () => {
+    let plant = {soil: 0, light: 0, water: 0 }
+    //const hydrate = changeState("water")(1);
+    expect(hydrate(plant)).toMatchObject({soil: 0, light: 0, water: 1});
+  });
 
   //light test
+  test ('increment "light" property by 1', () => {
+    let plant = {soil: 0, light: 0, water: 0 }
+    //const hydrate = changeState("water")(1);
+    expect(illuminate(plant)).toMatchObject({soil: 0, light: 1, water: 0});
+  });
 
 });
